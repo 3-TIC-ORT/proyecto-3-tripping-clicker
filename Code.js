d8f568a1f6aa5d804099cc71a1d9b094a37375c2
Kadoms <167230757+Kadoms@users.noreply.github.com>
2024-08-14 10:53:57 -0300

    simon

--- Code.js
+++ Code.js
@@ -1,7 +1,37 @@
 let puntos = 0
+let puntosxs = 0
+let poderclick = 1
+
 function maspuntos() {
-    puntos = puntos + 1
+    puntos = puntos + poderclick
+    console.log (puntos)
+}
+
+function mejoraunoprecio(){
+    if (puntos>=10) {
+        puntos = puntos-10
+        console.log (puntos,"puntos")
+        puntosxs = puntosxs+1
+        console.log (puntosxs, "pps")
+    }
+}
+
+function delay(timeInMs) {
+    return new Promise(resolve => setTimeout(resolve, timeInMs));
+}
+async function bucleiinfinito(){
+while (true) {
+
+    puntos = puntos + puntosxs
+    console.log (puntos)
+    await delay(1000)
     
 }
+}
+bucleiinfinito()
+
+
+document.getElementById("mejora1").addEventListener("click",mejoraunoprecio)
+
 
 document.getElementById("mainobjdiv").addEventListener("click",maspuntos);
