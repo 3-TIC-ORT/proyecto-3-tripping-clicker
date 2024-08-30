0a4f82c48d746b27d58ef24b93cb21668918fc83
simonch <167230757+Kadoms@users.noreply.github.com>
2024-08-30 14:59:20 -0300

    simon

--- Code2.js
+++ Code2.js
@@ -1,11 +1,9 @@
-import { guardarprogreso } from './'
-
-
 let puntosD = 0
 let puntosxs = 0
 let poderclick = 1
 let precio1 = 10
 let precio2 = 100
+Cargarprogreso()
 
 function maspuntos() {
     puntosD = puntosD + poderclick
@@ -16,7 +14,7 @@ function mejoraunoprecio() {
     if (puntosD >= precio1) {
         puntosD = puntosD - precio1
         precio1 = precio1 * 1.25
-        puntosxs = puntosxs + 0.001
+        puntosxs = puntosxs + 0.01
     }
 }
 
@@ -24,7 +22,7 @@ function mejoradosprecio() {
     if (puntosD >= precio2) {
         puntosD = puntosD - precio2
         precio2 = precio2 * 1.25
-        puntosxs = puntosxs + 0.01
+        puntosxs = puntosxs + 0.1
     }
 }
 
@@ -32,16 +30,18 @@ function delay(timeInMs) {
     return new Promise(resolve => setTimeout(resolve, timeInMs));
 }
 
-async function bucleiinfinito(){
-while (true) {
+async function bucleinfinito(){
+    while (true) {
 
-    puntosD = puntosD + puntosxs
-    document.getElementById("Puntos").innerHTML = `Puntos ${Math.ceil(puntosD)}`
-    await delay(10)
-    
+        puntosD = puntosD + puntosxs
+        puntosD = (puntosD)
+        document.getElementById("Puntos").innerHTML = `Puntos: ${Math.ceil (puntosD)}`
+        await delay(100)
+        console.log (puntosD)
+        
+    }
 }
-}
-bucleiinfinito()
+bucleinfinito()
 
 document.getElementById("mejora1").addEventListener("click",mejoraunoprecio);
 
@@ -50,15 +50,6 @@ document.getElementById("mejora2").addEventListener("click",mejoradosprecio);
 document.getElementById("mainobj").addEventListener("click",maspuntos);
 
 document.getElementById("guardar").addEventListener("click",guardarprogreso);
-
-document.getElementById("").addEventListener(Cargarprogreso)
-
-
-
-const {LocalStorage} = require("node-localstorage");
-
-const localstorage = new LocalStorage("./Users/49375882/Documents/GitHub/proyecto-3-tripping-clicker");
- 
 // Guardar
 
 function guardarprogreso() { 
@@ -68,8 +59,8 @@ function guardarprogreso() {
     localStorage.setItem("precio mejora 2",precio2);
 }
 
-//Exportar
-function Cargarprogreso () {
+//Importar
+async function Cargarprogreso () {
     localStorage.getItem("puntos",puntosD);
 localStorage.getItem("puntos por segundo",puntosxs);
 localStorage.getItem("precio mejora 1",precio1);
