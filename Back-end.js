e4264d9f91b5695d263bd615e0a2b06d04afc819
MarguJ <margulesjoa@gmail.com>
2024-10-14 09:30:11 -0300

    Update Back-end.js

--- Back-end.js
+++ Back-end.js
@@ -1,10 +1,10 @@
-import fs from "fs"
-import {onEvent, startServer} from "soquetic"
+import fs from "fs";
+import { onEvent, startServer } from "soquetic";
 
 // Guardar
-function guardarprogreso(dimension,puntosD,puntosxs,precio1,precio2,clicks,poderclick) {
+function guardarprogreso(dimension, puntosD, puntosxs, precio1, precio2, clicks, poderclick) {
     // Guardar progreso de la dimensión 1
-    if (dimension = 1) {
+    if (dimension === 1) {
         const guardado = {
             "puntos": puntosD,
             "puntos por segundo": puntosxs,
@@ -12,23 +12,23 @@ function guardarprogreso(dimension,puntosD,puntosxs,precio1,precio2,clicks,poder
             "mejora 2": precio2,
             "clicks": clicks,
             "poderclick": poderclick,
-        }
-        fs.writeFileSync(`./Datos/Guardado.json`,JSON.stringify(guardado,null,2)`utf8`);
+        };
+        fs.writeFileSync(`./Datos/Guardado.json`, JSON.stringify(guardado, null, 2), "utf8");
     }
     // Guardar progreso de la dimensión 2
-    else if (dimension = 2) {
-            const guardado2 = {
-                "puntos": puntosD,
-                "puntos por segundo": puntosxs,
-                "mejora 1": precio1,
-                "mejora 2": precio2,
-                "clicks": clicks,
-                "poderclick": poderclick,
-            }
-            fs.writeFileSync(`./Datos/Guardado2.json`,JSON.stringify(guardado2,null,2)`utf8`);
+    else if (dimension === 2) {
+        const guardado2 = {
+            "puntos": puntosD,
+            "puntos por segundo": puntosxs,
+            "mejora 1": precio1,
+            "mejora 2": precio2,
+            "clicks": clicks,
+            "poderclick": poderclick,
+        };
+        fs.writeFileSync(`./Datos/Guardado2.json`, JSON.stringify(guardado2, null, 2), "utf8");
     }
     // Guardar progreso de la dimensión 3
-    else if (dimension = 3) {
+    else if (dimension === 3) {
         const guardado3 = {
             "puntos": puntosD,
             "puntos por segundo": puntosxs,
@@ -36,39 +36,35 @@ function guardarprogreso(dimension,puntosD,puntosxs,precio1,precio2,clicks,poder
             "mejora 2": precio2,
             "clicks": clicks,
             "poderclick": poderclick,
-        }
-        fs.writeFileSync(`./Datos/Guardado3.json`,JSON.stringify(guardado3,null,2)`utf8`);
-    }
-    else {
-        alert("hiciste algo mal")
+        };
+        fs.writeFileSync(`./Datos/Guardado3.json`, JSON.stringify(guardado3, null, 2), "utf8");
+    } else {
+        console.error("hiciste algo mal");
     }
 }
 
-//Exportar
-
-function cargarprogreso (dimension) {
+// Exportar
+function cargarprogreso(dimension) {
     // Cargar progreso de la dimensión 1
-    if (dimension = 1) {
-        let progreso = JSON.parse(fs.readFleSync(`./Datos/Guardado.json`,"utf8"));
-        return progreso
+    if (dimension === 1) {
+        let progreso = JSON.parse(fs.readFileSync(`./Datos/Guardado.json`, "utf8"));
+        return progreso;
     }
     // Cargar progreso de la dimensión 2
-    else if (dimension = 2) {
-        let progreso2 = JSON.parse(fs.readFleSync(`./Datos/Guardado2.json`,"utf8"));
-        return progreso2
+    else if (dimension === 2) {
+        let progreso2 = JSON.parse(fs.readFileSync(`./Datos/Guardado2.json`, "utf8"));
+        return progreso2;
     }
     // Cargar progreso de la dimensión 3
-    else if (dimension = 3) {
-        let progreso3 = JSON.parse(fs.readFleSync(`./Datos/Guardado3.json`,"utf8"));
-        return progreso3
-    }
-    else {
-        alert("hiciste algo mal (pt 2)")
+    else if (dimension === 3) {
+        let progreso3 = JSON.parse(fs.readFileSync(`./Datos/Guardado3.json`, "utf8"));
+        return progreso3;
+    } else {
+        console.error("hiciste algo mal (pt 2)");
     }
 }
- 
-onEvent(`guardar`,guardarprogreso)
 
-onEvent(`cargar`,cargarprogreso)
+onEvent(`guardar`, guardarprogreso);
+onEvent(`cargar`, cargarprogreso);
 
-startServer()
+startServer();
