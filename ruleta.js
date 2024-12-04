b8cd6f12207703a58d0567430eb0b1a159aa279d
simonch <167230757+Kadoms@users.noreply.github.com>
2024-12-04 15:23:37 -0300

    cosas

--- ruleta.js
+++ ruleta.js
@@ -1,38 +1,52 @@
-let par = document.getElementById("par").value
-let impar = document.getElementById("impar").value
-let uno = document.getElementById("1").value
-let dos = document.getElementById("2").value
-let tres = document.getElementById("3").value
-let cuatro = document.getElementById("4").value
-let cinco = document.getElementById("5").value
-let seis = document.getElementById("6").value
-let siete = document.getElementById("7").value
-let ocho = document.getElementById("8").value
-let nueve = document.getElementById("9").value
-let diez = document.getElementById("10").value
-let once = document.getElementById("11").value
-let doce = document.getElementById("12").value
-let trece = document.getElementById("13").value
-let catorce = document.getElementById("14").value
-let quince = document.getElementById("15").value
-let dieciseis = document.getElementById("16").value
-let diecisiete = document.getElementById("17").value
-let dieciocho = document.getElementById("18").value
-let diecinueve = document.getElementById("19").value
-let veinte = document.getElementById("20").value
-let veintiuno = document.getElementById("21").value
-let veintidos = document.getElementById("22").value
-let veintitres = document.getElementById("23").value
-let veinticuatro = document.getElementById("24").value
-let veinticinco = document.getElementById("25").value
-let veintiseis = document.getElementById("26").value
-let veintisiete = document.getElementById("27").value
-let veintiocho = document.getElementById("28").value
-let veintinueve = document.getElementById("29").value
-let treinta = document.getElementById("30").value
-let treintayuno = document.getElementById("31").value
-let treintaydos = document.getElementById("32").value
-let treintaytres = document.getElementById("33").value
-let treintaycuatro = document.getElementById("34").value
-let treintaycinco = document.getElementById("35").value
-let treintayseis = document.getElementById("36").value
+let par = document.getElementById("par").value;
+let impar = document.getElementById("impar").value;
+let uno = document.getElementById("1").value;
+let dos = document.getElementById("2").value;
+let tres = document.getElementById("3").value;
+let cuatro = document.getElementById("4").value;
+let cinco = document.getElementById("5").value;
+let seis = document.getElementById("6").value;
+let siete = document.getElementById("7").value;
+let ocho = document.getElementById("8").value;
+let nueve = document.getElementById("9").value;
+let diez = document.getElementById("10").value;
+let once = document.getElementById("11").value;
+let doce = document.getElementById("12").value;
+let trece = document.getElementById("13").value;
+let catorce = document.getElementById("14").value;
+let quince = document.getElementById("15").value;
+let dieciseis = document.getElementById("16").value;
+let diecisiete = document.getElementById("17").value;
+let dieciocho = document.getElementById("18").value;
+let diecinueve = document.getElementById("19").value;
+let veinte = document.getElementById("20").value;
+let veintiuno = document.getElementById("21").value;
+let veintidos = document.getElementById("22").value;
+let veintitres = document.getElementById("23").value;
+let veinticuatro = document.getElementById("24").value;
+let veinticinco = document.getElementById("25").value;
+let veintiseis = document.getElementById("26").value;
+let veintisiete = document.getElementById("27").value;
+let veintiocho = document.getElementById("28").value;
+let veintinueve = document.getElementById("29").value;
+let treinta = document.getElementById("30").value;
+let treintayuno = document.getElementById("31").value;
+let treintaydos = document.getElementById("32").value;
+let treintaytres = document.getElementById("33").value;
+let treintaycuatro = document.getElementById("34").value;
+let treintaycinco = document.getElementById("35").value;
+let treintayseis = document.getElementById("36").value;
+
+
+
+let result = Math.floor(Math.random() * 36)+ 1;
+
+let i = 0
+let puntos 
+
+while (i >= 35){
+    i++
+    if (document.getElementById(i).value > 0){
+    puntos = puntos + document.getElementById.value * 36
+    }
+}
